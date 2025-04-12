7c12817778d0cc28e68edecebc7f031c14930d5c
vitoriajaco <vitoriajaco@gmail.com>
2025-04-12 17:53:26 -0300

    Fix: Alterando rota api e acrescentando logradouro

--- src/main/java/com/api/cpf/controller/EnderecoController.java
+++ src/main/java/com/api/cpf/controller/EnderecoController.java
@@ -3,21 +3,22 @@ package com.api.cpf.controller;
 import com.api.cpf.client.EnderecoService;
 import com.api.cpf.response.Endereco;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api")
-public class ApiController {
+@RequestMapping("/buscar")
+public class EnderecoController {
 
     @Autowired
     EnderecoService enderecoService;
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cep}")
     public ResponseEntity<Endereco> retornaEndereco(@PathVariable String cep){
+        if (cep == null || cep.isEmpty()) {
+            throw new IllegalArgumentException("O CEP não pode ser nulo ou vazio.");
+        }
         Endereco endereco = enderecoService.getAddressByCep(cep);
 
         return ResponseEntity.ok(endereco);
--- src/main/java/com/api/cpf/response/Endereco.java
+++ src/main/java/com/api/cpf/response/Endereco.java
@@ -5,46 +5,71 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Endereco {
 
+    private String logradouro;
     private String localidade;
     private String uf;
-
+    private String bairro;
     private String cep;
 
-    private String bairro;
-
-    public Endereco() {
-    }
-
-    public Endereco(String localidade, String uf, String cep, String bairro) {
+    public Endereco(String logradouro, String localidade, String uf, String bairro, String cep) {
+        this.logradouro = logradouro;
         this.localidade = localidade;
         this.uf = uf;
-        this.cep = cep;
         this.bairro = bairro;
+        this.cep = cep;
+    }
+
+    public Endereco() {
     }
 
     @Override
     public String toString() {
         return "Endereco{" +
-                "localidade='" + localidade + '\'' +
+                "logradouro='" + logradouro + '\'' +
+                ", localidade='" + localidade + '\'' +
                 ", uf='" + uf + '\'' +
-                ", cep='" + cep + '\'' +
                 ", bairro='" + bairro + '\'' +
+                ", cep='" + cep + '\'' +
                 '}';
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
     }
 
     public String getLocalidade() {
         return localidade;
     }
 
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
     public String getUf() {
         return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
     }
 
     public String getCep() {
         return cep;
     }
 
-    public String getBairro() {
-        return bairro;
+    public void setCep(String cep) {
+        this.cep = cep;
     }
 }
